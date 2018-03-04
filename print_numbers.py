b1b7e131b37cc7db42a98f113eb2a85deca0c740
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-03-04 13:45:00 +0200

    add print_numbers function info

--- print_numbers.py
+++ print_numbers.py
@@ -1,6 +1,21 @@
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints 
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+
 def print_numbers(final):
-    number=1
+    number = 1
     while number <= final:
         print number
-        number+=1
+        number += 1
+
+
 print_numbers(3)
+
+# >>> 1
+# >>> 2
+# >>> 3
