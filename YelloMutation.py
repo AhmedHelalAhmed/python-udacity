c715bd290a4b203da9f60fe51ef12caf1f489cac
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-03-09 20:15:39 +0200

    Yello Mutation

---
+++ YelloMutation.py
@@ -0,0 +1,23 @@
+#Yello Mutation
+#for string
+str="hello"
+print str[0]
+#str[0]="y" ---> error immutable
+#=========================#
+#for list
+#reference to the same object
+#reference
+p=['H','E','l','l','o']
+q=p
+print p
+print q
+p[0]='Y'
+print p
+print q
+#==========================#
+
+
+
+
+
+
