5ee9e79b7c542705ba7c349b2c15cc647127f0c7
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-03-18 18:25:32 +0200

    modify the message

--- Send Text Messages/SendSMS.py
+++ Send Text Messages/SendSMS.py
@@ -5,12 +5,14 @@ account_sid = "AC79abc185f7b09d093e1f6ba16fb19e37"
 # Your Auth Token from twilio.com/console
 auth_token  = "28c28276b2949d99826165415503955e"
 
-client = Client(account_sid, auth_token)
+client = Client(account_sid,
+
+                auth_token)
 
 message = client.messages.create(
     to="+2***********", #your number
     from_="+12163501159", #twilio number
-    body=". . . Diaa test test")
+    body=". . . Python test test")
 
 print(message.sid)
 
